6e94af6d604c3e4e45d9915328f53138ac61a37f
Anjalirajput837 <anjalirajput5051@gmail.com>
2025-11-17 11:02:02 +0530

    Remove duplicate Fibonacci check in loop

--- home-page1.py
+++ home-page1.py
@@ -10,5 +10,7 @@ def is_fibonacci(n):
 for i in range(1, 7):
     if is_fibonacci(i):
         print(f"{i} is a Fibonacci Number")
+    if is_fibonacci(i):
+        print(f"{i} is a Fibonacci Number")
     else:
         print(f"{i} is not a Fibonacci Number")
