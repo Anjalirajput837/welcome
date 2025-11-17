b91b3fa9e08fa7445363adb4b81b9b76de37b7b8
Anjali <anjalirajput5051@gmail.com>
2025-11-17 11:05:43 +0530

    Third commit

--- home-page1.py
+++ home-page1.py
@@ -12,5 +12,7 @@ for i in range(1, 7):
         print(f"{i} is a Fibonacci Number")
      else:
         print(f"{i} is not a Fibonacci Number")
+            if is_fibonacci(i):
+
     else:
         print(f"{i} is not a Fibonacci Number")
